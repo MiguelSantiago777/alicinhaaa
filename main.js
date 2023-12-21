d43538d9863e1014632b71db4de497926e8d3e85
Miguel Santiago <72167799+MiguelSantiago777@users.noreply.github.com>
2023-12-21 15:20:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+alert('Olá você acaba de começar o quizz do namorado!');
+alert('consiste em 3 perguntas para você digitar a resposta, então vamos lá!');
+prompt('Promete me amar pelo resto da vida?');
+prompt('promete me dar infinitos beijinhos pra sempre?');
+prompt('Me ama mais que o botafogo?');
